364ee507bf34cf0e01c606280ed1856fa86427d2
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2020-09-01 01:05:13 +0000

    Strech project work 4-Programming Intro of Compass

--- evaluation-problems-arrays.js
+++ evaluation-problems-arrays.js
@@ -57,6 +57,36 @@ console.log(lastIndexOf([], 3), "=?", -1);
 
 // Loopy Lighthouse 2 stretch project
 
+let loopyLighthouse = function(range, multiples, words) {
+  
+  for (let i = range[0]; i <= range[1]; i++) {
+
+    if (i % multiples[0] === 0 && i % multiples[1] === 0) {
+  
+      console.log(words[0] + words[1]);
+  
+    } else if (i % multiples[0] === 0) {
+  
+      console.log(words[0]);
+  
+    } else if (i % multiples[1] === 0) {
+  
+      console.log(words[1]);
+  
+    } else {
+    
+      console.log(i);
+  
+    }
+  
+  }
+
+}
+/* Long Output of test for above function commented out
+
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
+*/
+
 // Concatenate Arrays
 
 
@@ -92,3 +122,44 @@ console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
 console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
 
 // Merge Arrays stretch project
+
+
+  
+function merge(array1,array2) {
+  let mergedArray = [];
+
+  while (array1.length && array2.length) {
+
+    if(array1[0] <= array2[0]) {
+
+      mergedArray.push(array1[0]);
+      array1 = array1.slice(1);
+
+    } else {
+
+      mergedArray.push(array2[0]);
+      array2 = array2.slice(1);
+
+    }
+
+  }
+  
+  while (array1.length)
+
+    mergedArray.push(array1.shift());
+
+  while (array2.length)
+
+    mergedArray.push(array2.shift());
+
+  return mergedArray;
+
+
+} 
+
+
+
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
